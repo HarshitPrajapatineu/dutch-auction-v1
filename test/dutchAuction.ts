011693bf55f873a20975e47e7b02a076c1fa92ea
Harshit <prajapati.ha@northeastern.edu>
2023-06-15 00:50:28 -0400

    test cases issue fixed

--- test/dutchAuction.ts
+++ test/dutchAuction.ts
@@ -3,6 +3,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+const provider = ethers.provider;
 describe("BasicDutchAuction", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
@@ -19,15 +20,17 @@ describe("BasicDutchAuction", function () {
 
     const BasicDutchAuction = await ethers.getContractFactory("BasicDutchAuction");
     const basic_dutch_auction = await BasicDutchAuction.deploy(reservePrice, numBlocksAuctionOpen, offerPriceDecrement);
-
+    console.log(owner.getAddress);
+    const balance = await provider.getBalance(owner.address);
+    console.log(balance + "adcawescaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     return { basic_dutch_auction, reservePrice, numBlocksAuctionOpen, offerPriceDecrement, owner, account1, account2, account3, account4 };
   }
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
       const { basic_dutch_auction, owner, account1 } = await loadFixture(deployBasicDutchAuction);
-      console.log(await owner.getBalance(),"balanceee");
-      console.log(ethers.utils.formatEther(await account1.getBalance()),"balanceee");
+      console.log(await provider.getBalance(owner.address),"balanceee");
+      console.log(ethers.formatEther(await provider.getBalance(account1.address)),"balanceee");
       
       
 
@@ -57,19 +60,21 @@ describe("Auction", function () {
     it("Buyers will bid and bid will be refunded", async function () {
       const { basic_dutch_auction,owner, account1, account2, account3, account4 } = await loadFixture(deployBasicDutchAuction);
       const bid1 = await basic_dutch_auction.connect(account1).bid({value: '100'});
-      const receipt1 = await bid1.wait()
-      const gasSpent1 = receipt1.gasUsed.mul(receipt1.effectiveGasPrice)
-      expect(await account1.getBalance()).to.eq(ethers.utils.parseEther("10000").sub(gasSpent1))
+      const receipt1 = await bid1.wait();
+      const gasSpent1 = receipt1!.gasUsed * (receipt1!.gasPrice);
+      console.log(gasSpent1 + "111111111111111111111111111111111111111111111111111111111111111111111111111");
+      expect(await provider.getBalance(account1.address)).to.eq(ethers.parseEther("10000") - (gasSpent1))
     });
 
     it("Buyers will bid and bid will be accepted", async function () {
         const { basic_dutch_auction,owner, account1, account2, account3, account4 } = await loadFixture(deployBasicDutchAuction);
-        const balance_before = await owner.getBalance();
+        const balance_before = await provider.getBalance(owner.address);
         const bid3 = await basic_dutch_auction.connect(account3).bid({value: '50000'});
-        const receipt3 = await bid3.wait()
-        const gasSpent3 = receipt3.gasUsed.mul(receipt3.effectiveGasPrice)
-        expect(await account3.getBalance()).to.eq(ethers.utils.parseEther("10000").sub(gasSpent3.add('50000')))
-        expect (await owner.getBalance()).to.eq(balance_before.add(50000));
+        const receipt3 = await bid3.wait();
+        console.log(receipt3)
+        const gasSpent3 = receipt3!.gasUsed *(receipt3!.gasPrice);
+        expect(await provider.getBalance(account3.address)).to.eq((ethers.parseEther("10000") - ((gasSpent3) + BigInt('50000'))).toString())
+        expect (await provider.getBalance(owner.address)).to.eq((balance_before + BigInt(50000)).toString());
       });
 
       it("Buyers can not bid after auction ended", async function () {
